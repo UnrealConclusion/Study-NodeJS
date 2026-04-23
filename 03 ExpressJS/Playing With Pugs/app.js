2d5e67e4479c73d1bdc803cbbbaf7dd6e311a7b5
Jacob Lin <jacoblin@Jacobs-MacBook-Air.local>
2026-04-23 01:28:15 -0400

    setup complete

---
+++ 03 ExpressJS/Playing With Pugs/app.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// configure express to use Pug
+app.set('view engine', 'pug');
+app.set('view', 'views'); // default
+
+// use body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+// serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// import and mount routes 
+const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+app.use(shopRoutes);
+app.use(adminRoutes);
+
+app.listen(3000);
