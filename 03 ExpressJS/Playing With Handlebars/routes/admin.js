6f2a6736a8e28f579f02560a1c26c1c9c3e6ded9
Jacob Lin <jacoblin@Jacobs-MacBook-Air.local>
2026-04-27 20:13:46 -0400

    Finished playing with handlebars

--- 03 ExpressJS/Playing With Handlebars/routes/admin.js
+++ 03 ExpressJS/Playing With Handlebars/routes/admin.js
@@ -5,18 +5,32 @@ const products = []; // global products list (for demonstration purposes)
 
 // get pug
 router.get('/add-product', (request, response, next) => {
-  response.render('addproduct', { title: 'Add Product', page: 'product' });
+  response.render('addproduct', 
+	  { 
+		title: 'Add Product', 
+		page: 'product',
+		layout: 'main',
+		activeShop: false,
+		activeAdd: true
+	  });
 });
 
 // post pug
 router.post('/add-product', (request, response, next) => {
-  products.push({ item: request.body.item });
-  response.redirect('/');
+	products.push({ item: request.body.item });
+	response.redirect('/');
 });
 
 // 404 
 router.use('/', (request, response, next) => {
-  response.render('404', {title: "Not Found", page: '404'});
+  response.render('404', 
+	{
+		title: "Not Found", 
+		page: '404',
+		layout: 'main',
+		activeShop: false,
+		activeAdd: false
+	});
 });
 
 
--- 03 ExpressJS/Playing With Handlebars/routes/shop.js
+++ 03 ExpressJS/Playing With Handlebars/routes/shop.js
@@ -5,7 +5,17 @@ const data = require('./admin');
 
 router.get('/', (req, res, next) => {
   const products = data.products;
-  res.render('shop', {title: 'Shop', page: 'home', products: products});
+  console.log(products);
+  res.render('shop', 
+	  {
+		  title: 'Shop', 
+		  page: 'home', 
+		  products: products,
+		  layout: 'main',
+		  activeShop: true,
+		  activeAdd: false,
+		  hasProducts: products.length > 0
+		});
 });
 
 module.exports = router;
